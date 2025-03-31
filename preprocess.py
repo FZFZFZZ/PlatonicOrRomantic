7bcd7c78aa4a289f7241ba7520b09b080bc2ce64
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2025-03-31 08:59:50 +0800

    Encode pauses

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,9 @@ import pickle
 import sys
 
 Role = Literal[-1, 1]
-Text = tuple[Role, np.ndarray]  # array shape: (L, V) where L is number of tokens, V is vector size (currently 50)
+# array shape: (L, V) where L is number of tokens, V is vector size (currently 50)
+# None means a long pause
+Text = tuple[Role, np.ndarray | None]
 Dialog = list[Text]
 Label = Literal[-1, 0, 1]
 
@@ -33,13 +35,13 @@ def preprocess_data(embeddings_file: str, out: str, file: str = "Data/train.csv"
         label = row['Label']
         dialogue_data: Dialog = []
         for text in dialogue:
+            role = 1 if text['role'] == 'A' else -1
             if text['response'] == '$S$':
-                continue
+                dialogue_data.append((role, None))
             if text['response'].strip() == '':
                 continue
             if text['response'] == '$EXIT$':
                 break
-            role = 1 if text['role'] == 'A' else -1
             doc = nlp(text['response'])
             embeddings_data: list[np.ndarray] = []
             for token in doc:
